e8c4d70f200cace4f04caaefff2c441914aba504
dmvanveen-uu <d.m.vanveen@students.uu.nl>
2026-03-03 16:09:57 +0100

    Update Project 2.py

--- Project 2.py
+++ Project 2.py
@@ -1,3 +1,7 @@
 print("Hello world!")
+print("Hello Omar and Jeroen!")
 
-
+# Lets save the data in this format
+# names = [name1, name2, name3]
+# tables = [table1, table2, table3]
+#  with table1 = [name of person in seat1, name of person in seat2, etc]
