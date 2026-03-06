d29a7f6e8a0b40f2fc3c8554458f39a52467eeca
jeroen133cc <jeroen133cc@gmail.com>
2026-03-06 11:16:06 +0100

    Update Project 2.py

--- Project 2.py
+++ Project 2.py
@@ -2,6 +2,7 @@
 
 # CHANGELOG:
 
+#v1.3 Jeroen added instructions to fill out and import the Google Form and CSV file    
 #v1.2 Jeroen added link to google form
 #v1.1 Jeroen added parts of the existing code into this file
 # v1: Daan created a new import function
@@ -26,10 +27,21 @@ import os
 # Start by presenting the Google form to the user to fill out
 
 print("Welcome to our matching algorithm! To start, you can share this form to fill out by the users.")
-print("After filling this form out, please hit enter to receive the link to download the CSV file")
 print("https://docs.google.com/forms/d/e/1FAIpQLSd6CeVgLXvRE5YGlkfYEkanezgUMk_-0mmGZOGv39igk2e7Lg/viewform?usp=dialog")
+print("After filling this form out, please hit enter to receive the link to download the CSV file")
 
-#
+print("This Google Sheets doc below collected all the data from the Google Sheets mentioned above")
+print("You can import the .CSV file by:
+print(" --> opening the link below")
+print("--> Go to file in the top left corner")
+print("--> Go to download")
+print("--> select the "seperated by comma's" .csv option')
+print("--> The file is now downloaded to your computer")
+print("--> Make sure the .csv file is in the same directory (folder) as this script")
+print("! Remember the file name, you will be asked to input the filename later in this script. A typo will result in an error in the script")
+
+print("https://docs.google.com/spreadsheets/d/1PpeRo5NwWtCWgkyHn9dRoUrztqWxIX0mFlPERe4WtIU/edit?usp=sharing")
+
 
 
 
